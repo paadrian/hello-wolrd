e43613a44cfd6eabd3d6404539533540cc65e457
Raylander <adrianpanaite95@gmail.com>
2022-02-01 19:36:34 +0200

    Change application to library Add unit tests

---
+++ Greatings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Greatings
+    {
+        private readonly Action<string> _outputProvider;
+
+        public Greatings(Action<string> outputProvider)
+        {
+            _outputProvider = outputProvider;
+        }
+
+        public void SayHello()
+        {
+            _outputProvider("Hello, World!");
+            _outputProvider("Bonjour le monde!");
+            _outputProvider("Buna ziua lume!");
+            _outputProvider("Hallo Welt!");
+        }
+    }
+}
--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Bonjour le monde!");
-Console.WriteLine("Buna ziua lume!");
-Console.WriteLine("Hallo Welt!");
