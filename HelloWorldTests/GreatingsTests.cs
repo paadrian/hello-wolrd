11e0cda41957f4b26788001421958ff2bae0a8fd
Raylander <adrianpanaite95@gmail.com>
2022-02-08 11:32:25 +0200

    Change indentation to tabs of 4

--- HelloWorldTests/GreatingsTests.cs
+++ HelloWorldTests/GreatingsTests.cs
@@ -1,36 +1,31 @@
-﻿using Xunit;
-using HelloWorld;
+﻿using FluentAssertions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
+using Xunit;
 using SystemUnderTest = HelloWorld.Greatings;
-using FluentAssertions;
 
 namespace HelloWorld.Tests
 {
-    public class GreatingsTests
-    {
-        [Fact()]
-        public void SayHello_ShouldWriteGreetingsIn4Language_Always()
-        {
+	public class GreatingsTests
+	{
+		[Fact()]
+		public void SayHello_ShouldWriteGreetingsIn4Language_Always()
+		{
 			// arrange
-            var expectedResult = string.Join(Environment.NewLine, "Hello, World!", "Bonjour le monde!",
-                "Buna ziua lume!", "Hallo Welt!", "");
+			var expectedResult = string.Join(Environment.NewLine, "Hello, World!", "Bonjour le monde!",
+				"Buna ziua lume!", "Hallo Welt!", "");
 
-            var inMemoryOutput = new StringBuilder();
+			var inMemoryOutput = new StringBuilder();
 			var inMemoryOutputProvider = (string text) => { inMemoryOutput.AppendLine(text); };
 
-            var systemUnderTest = new SystemUnderTest(inMemoryOutputProvider);
+			var systemUnderTest = new SystemUnderTest(inMemoryOutputProvider);
 			//act
 
-			var sayHello = systemUnderTest.Invoking(sut => sut.SayHello()); 
+			var sayHello = systemUnderTest.Invoking(sut => sut.SayHello());
 
 			// assert
 			sayHello.Should().NotThrow();
 			var result = inMemoryOutput.ToString().Should().Be(expectedResult);
-        }
-    }
+		}
+	}
 }
